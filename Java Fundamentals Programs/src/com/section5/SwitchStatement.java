9cd7e0f7c0ebcf84c8db8dca21063a35bb1ccbdf
Rick Mansfield <mansfieldrick.business@gmail.com>
2020-12-01 19:44:18 -0600

    Completed a few lessons and finished coding exercise 12

--- Java Fundamentals Programs/src/com/section5/SwitchStatement.java
+++ Java Fundamentals Programs/src/com/section5/SwitchStatement.java
@@ -31,6 +31,54 @@ public class SwitchStatement {
                 System.out.println("Was not 1 or 2");
                 break;
         }
-    }
 
+        /*
+        CHALLENGE:
+         Create a new switch statement using char instead of int
+         create a new char variable
+         create a switch statement testing for
+         A, B, C, D, or E
+         display a message if any of these are found and then break
+         Add a default which displays a message saying not found
+         */
+
+        char character = 'A';
+
+        switch (character) {
+            case 'A':
+                System.out.println("Letter A was found.");
+                break;
+            case 'B':
+                System.out.println("Letter B was found.");
+                break;
+            case 'C':
+                System.out.println("Letter C was found");
+                break;
+            case 'D':
+                System.out.println("Letter D was found.");
+                break;
+            case 'E':
+                System.out.println("Letter E was found.");
+                break;
+            default:
+                System.out.println("Letters A-E were not found");
+                break;
+        }
+
+        // Switches with Strings:
+
+        String month = "JanUary";
+
+        switch (month.toLowerCase()) {
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("June");
+                break;
+            default:
+                System.out.println("What");
+                break;
+        }
+    }
 }
