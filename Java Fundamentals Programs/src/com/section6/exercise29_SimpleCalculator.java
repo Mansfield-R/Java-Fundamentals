c1a0a1297d5f720944832f9b46003e6cd5db7e65
Rick Mansfield <mansfieldrick.business@gmail.com>
2020-12-07 20:42:52 -0600

    Start of OOPS concepts (section 6); Objects and getters and setters

---
+++ Java Fundamentals Programs/src/com/section6/exercise29_SimpleCalculator.java
@@ -0,0 +1,51 @@
+package com.section6;
+
+public class exercise29_SimpleCalculator {
+
+    // Object Needs two fields:
+    private double firstNumber;
+    private double secondNumber;
+
+    // Getter and Setter Methods:
+    /*
+    We use setter methods to be able to give access to outside classes to create an instance of our object here and set their own fields
+    We use getter methods to return the fields that were set in the setter.
+    Open-Closed principle?????!?!?! As part of SOLID???!?!?!
+     */
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getFirstNumber() {
+        return this.firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getSecondNumber() {
+        return this.secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return (firstNumber + secondNumber);
+    }
+
+    public double getSubtractionResult() {
+        return (firstNumber - secondNumber);
+    }
+
+    public double getMultiplicationResult() {
+        return (firstNumber * secondNumber);
+    }
+
+    public double getDivisionResult() {
+        if (secondNumber == 0) {
+            return 0.0;
+        } else {
+            return (firstNumber / secondNumber);
+        }
+    }
+
+}
