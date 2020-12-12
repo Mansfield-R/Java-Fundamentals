7ba7683002846368edfb602702287a082eb07b3d
Rick Mansfield <mansfieldrick.business@gmail.com>
2020-12-12 11:50:28 -0600

    Creation of bank account objects and lesson of constructors.

---
+++ Java Fundamentals Programs/src/com/section6/VipCustomer.java
@@ -0,0 +1,35 @@
+package com.section6;
+
+public class VipCustomer {
+
+    private String name;
+    private double creditLimit;
+    private String emailAddress;
+
+    public VipCustomer() {
+        this("customer", 50000.00, "default.email@default.com");
+    }
+
+    public VipCustomer(String name, double creditLimit) {
+        this(name, creditLimit, "unknown@email.com");
+    }
+
+    public VipCustomer(String name, double creditLimit, String emailAddress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+
+    // Getters for this class:
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+}
